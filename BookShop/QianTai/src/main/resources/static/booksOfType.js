134edb43cc7eff11903adfdd541eec7aa677e2b7
woniumrwang <woniumrwang@qq.com>
2023-04-26 21:52:50 +0800

    前台  分类查看图书页 原型

---
+++ BookShop/QianTai/src/main/resources/static/booksOfType.js
@@ -0,0 +1,17 @@
+
+let booksOfTypeVue = new Vue({
+  el: "#booksOfType",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  created() {
+    console.log("booksOfTypeVue 对象创建完成了")
+
+  }
+
+});
