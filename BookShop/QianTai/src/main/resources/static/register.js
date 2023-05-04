e0d5666200b1135860a3d003917425d36c3d045a
woniumrwang <woniumrwang@qq.com>
2023-05-04 11:26:08 +0800

    前台 注册  前端原型

---
+++ BookShop/QianTai/src/main/resources/static/register.js
@@ -0,0 +1,18 @@
+
+let registerVue = new Vue({
+  el: "#register",
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+
+  },
+  created() {
+    console.log("registerVue 对象创建完成了")
+
+  }
+
+});
