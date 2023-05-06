c328f775d63f132c14aaf2699f75735a2d3390f7
woniumrwang <woniumrwang@qq.com>
2023-05-06 10:59:20 +0800

    RabbitMQ  demo2实验环境

---
+++ Demos/RabbitMQDemo2/src/main/java/com/woniuxy/RabbitMQDemo2Main.java
@@ -0,0 +1,14 @@
+package com.woniuxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMQDemo2Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMQDemo2Main.class,args);
+    }
+
+
+}
