48e79ae742fe86ba8d4602b2d6ab29f45cd55e78
woniumrwang <woniumrwang@qq.com>
2023-05-08 11:29:17 +0800

    前台  购物车 新增地址 省市区三级联动

--- BookShop/QianTai/src/main/resources/static/orderPreview.js
+++ BookShop/QianTai/src/main/resources/static/orderPreview.js
@@ -1,5 +1,5 @@
 
-let orderPreview = new Vue({
+let orderPreviewVue = new Vue({
   el: "#orderPreview",
   data() {
     return {
@@ -16,15 +16,61 @@ let orderPreview = new Vue({
         detail: '地址CCCCCCCCCC'
       }],
       //选中的购物项
-      cartItems:[]
-      ,
+      cartItems:[],
+      totalPrice:"0.00",
       //新增地址模态框相关的
       dialogFormVisible: false,
       closeOnClickModal: false,
-      totalPrice:"0.00"
+      addAddressForm:{
+        province:'',
+        city:'',
+        area:'',
+        detailAddress:'',
+        tel:'',
+        reciver:'',
+        emailCode:'',
+        isDefault:'0'
+      },
+      provinceList:[],
+      provinceId:-1,
+      cityList:[],
+      cityId:-1,
+      areaList:[],
+      areaId:-1
     }
   },
   methods: {
+    initProvinceLsit(){
+      axios.postForm("/address/queryByPid",{Pid:0})
+          .then( response => {
+            orderPreviewVue.provinceList = response.data;
+          })
+    },
+    reloadCityList(){
+      //复原子一级的数据
+      this.cityList=[]
+      this.cityId=-1
+      this.areaList=[]
+      this.areaId=-1
+
+      axios.postForm("/address/queryByPid",{Pid:this.provinceId})
+          .then( response => {
+            orderPreviewVue.cityList = response.data;
+          })
+
+    }
+    ,
+    reloadAreaList(){
+      this.areaList=[]
+      this.areaId=-1
+
+
+      axios.postForm("/address/queryByPid",{Pid:this.cityId})
+          .then( response => {
+            orderPreviewVue.areaList = response.data;
+          })
+
+    }
 
   },
   created() {
@@ -39,6 +85,10 @@ let orderPreview = new Vue({
     this.totalPrice = totalPrice;
 
     console.log("orderPreviewVue 对象创建完成了")
+
+    //初始化数据
+    this.initProvinceLsit();
+
   }
 
 });
