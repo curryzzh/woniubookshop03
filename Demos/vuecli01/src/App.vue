bd32198181ff6b0da08ffdf80f435e935b41faf7
woniumrwang <woniumrwang@qq.com>
2023-05-09 15:33:55 +0800

    vueCli  自定义User组件

--- Demos/vuecli01/src/App.vue
+++ Demos/vuecli01/src/App.vue
@@ -2,18 +2,21 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome 2222 to Your Vue.js App"/>
+    <User name="蜗牛" age="18"/>
   </div>
 </template>
 
 <script>
 //导入其它组件
 import HelloWorld from './components/HelloWorld.vue'
+import User from "@/components/User.vue";
 
 //导出当前文件为一个组件
 export default {
   name: 'App',  //名字
   components: {  //声明引用的外部组件
     HelloWorld   //例如这里是使用的HelloWorld组件 等效于 HelloWorld:HelloWorld
+    ,User
   }
 }
 </script>
---
+++ Demos/vuecli01/src/components/User.vue
@@ -0,0 +1,46 @@
+<template>
+  <!--页面模板 template下只能有一个根元素 -->
+  <div>
+    我是User  我叫{{name}}  我{{age}}岁了
+
+    <p>{{detail}}</p>
+    <br/>
+
+    <button @click="showDetail">showDetail</button>
+  </div>
+
+</template>
+
+<script>
+// js代码
+export default {
+  name: "User",
+  props: {  //声明组件的属性,需要应用该组件的地方指定属性值  props是properties的缩写
+    name: String,
+    age: Number
+  },
+  data(){
+
+    return {
+       detail:"我在盘石软件园等你呦~"
+    }
+  }
+  ,
+  methods:{
+    showDetail(){
+      alert(this.detail)
+    }
+  }
+  ,
+  created() {
+    console.log("User组件创建了......")
+  }
+}
+</script>
+
+<style scoped>
+
+/*css样式*/
+
+
+</style>
